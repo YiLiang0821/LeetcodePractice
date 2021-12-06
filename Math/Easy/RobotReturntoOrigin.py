3773bd727d8c2901a6497be24500e8db18fd00d3
Chen <ylchen@909c4acf1cab.ant.amazon.com>
2021-12-06 17:45:11 +0800

    2021/12/01

---
+++ Math/Easy/RobotReturntoOrigin.py
@@ -0,0 +1,9 @@
+def judgeCircle(moves):
+    if moves.count('U') != moves.count('D') or moves.count('L') != moves.count('R'):
+        return False
+    return True
+
+'''
+TC is O(4 * n) = O(n)
+SC is O(1)
+'''
