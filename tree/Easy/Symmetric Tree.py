da7571ed190e07a01803e1849becbfda5342e5e5
Chen <ylchen@909c4acf1cab.ant.amazon.com>
2021-12-21 20:14:55 +0800

    21

--- tree/Easy/Symmetric Tree.py
+++ tree/Easy/Symmetric Tree.py
@@ -1,3 +1,18 @@
 #101
 # left subtree.left == right subtree.right
 # left subtree.right == right subree.left
+def isMirror(t1, t2):
+    if(t1 is None and t2 is None):
+        return True
+    if (t1 is None or t2 is None):
+        return False
+    return (t1.val == t2.val) and isMirror(t1.left, t2.right) and isMirror(t1.right, t2.left)
+
+def isSymmetric(root):
+    isMirror(root, root)
+
+
+'''
+TC is O(n), visited all nodes
+SC us O(n), recursion call stack
+'''
